ccd848c171c7e3b846480908f32ae580fe7b91ea
zhao <zhao_jian_hao@163.com>
2025-04-02 19:01:02 +0800

    Update manage.py
    添加了一个测试类

--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+
+class Test:
+    def __init__(self,name):
+        self.name=name
+
+    def __str__(self):
+        return f"这是我的名字：{self.name}"
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myweb.settings")
 
